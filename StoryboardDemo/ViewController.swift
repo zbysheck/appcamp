b159b6d6b476646a194cbcd8a0d485d59a3f6e85
appacmp <appacmp@appacmps-MacBook-Pro.local>
2015-07-18 09:18:05 +0200

    Initial Commit

---
+++ StoryboardDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryboardDemo
+//
+//  Created by appacmp on 18/07/15.
+//  Copyright (c) 2015 appacmp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
